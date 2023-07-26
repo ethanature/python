f8c3ce9772629b401b09a23a0bea09c0731604f7
Ethan <138442555+ethanature@users.noreply.github.com>
2023-07-26 12:20:28 +0400

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,5 +1,5 @@
 num = int(input("Enter a number: "))  # your number
-factorial = 1
+factorial = 1  # var name - factorial
 for i in range(1, num + 1):
     factorial = factorial * i
 print("The factorial of", num, "is", factorial)
