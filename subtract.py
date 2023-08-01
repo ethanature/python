90be66e941c32215a153386630938e33ff917d0c
Ethan <138442555+ethanature@users.noreply.github.com>
2023-08-01 20:26:10 +0400

    Update subtract.py

--- subtract.py
+++ subtract.py
@@ -1,4 +1,5 @@
-num1 = int(input())
-num2 = int(input())
+num1 = int(input())  # input your first digit
+num2 = int(input())  # input your second digit
+
 difference = num1 - num2
 print("The difference between", num1, "and", num2, "is", difference)
