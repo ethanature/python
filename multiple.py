583e0c8237329003c1b10968a85d98371e1bd1b8
Ethan <138442555+ethanature@users.noreply.github.com>
2023-08-08 20:49:55 +0400

    Update multiple.py

--- multiple.py
+++ multiple.py
@@ -1,5 +1,7 @@
 print('Write down two numbers (each on new string)')
 num1 = input()
 num2 = input()
-product = num1 * num2
+
+product = num1 * num2  # multiple two variables
+
 print("The product of", num1, "and", num2, "is", product)
